be5c614f53ddf2a149521763a782807bfe56053a
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2022-12-07 14:16:24 -0500

    Add prettier for formating.

--- components/query-builder/query-builder.js
+++ components/query-builder/query-builder.js
@@ -1,20 +1,24 @@
 // /geo?north=76.26869465080624&east=-32.69531250000001&south=28.459033019728068&west=-167.34375000000003&keyword=blood native&lang=en&min=1&max=10&theme=economy
-const query = function(keywords) {
-    return 'SELECT properties[1].id AS id FROM v2_25 LIMIT 3'
-}
+const query = function (keywords) {
+  return "SELECT properties[1].id AS id FROM v2_25 LIMIT 3";
+};
 
 // keywordsArray sample: ["Blood in the water", "mountain", "nonce"]
-const whereClause = function(keywordsArray) {
-    if (!keywordsArray) return ""
-    let ret = "where"
-    keywordsArray.forEach(e => {
-        ret += ' and ' + e.toLowerCase()
-    }    
-)
-    return ret
-}
+const whereClause = function (keywordsArray) {
+  if (!keywordsArray) return "";
+  let ret = "where";
+  const firstItem = true;
+  keywordsArray.forEach((e) => {
+    if (firstItem) {
+      ret += " and " + e.toLowerCase();
+    } else {
+      ret += e.toLowerCase();
+    }
+  });
+  return ret;
+};
 
 module.exports = {
-    query,
-    whereClause
-}
+  query,
+  whereClause,
+};
--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 //import AthenaExpress from "athena-express";
 //import * as AWS from "@aws-sdk/client-athena";
 
-const AthenaExpress = require("athena-express")
-const AWS = require("aws-sdk")
-const { query } = require('compoents/query-builder/query-builder.js')
+const AthenaExpress = require("athena-express");
+const AWS = require("aws-sdk");
+const { query } = require("compoents/query-builder/query-builder.js");
 
 /* AWS Credentials are not required here 
     /* Make sure the IAM Execution Role used by this Lambda 
@@ -13,25 +13,24 @@ const { query } = require('compoents/query-builder/query-builder.js')
     /* and store the result in Amazon S3 bucket
     /* See configuration section above under Setup for more info */
 const athenaExpressConfig = {
-    aws: AWS,
-    db: "geocore_metadata",
-    formatJson: true,
-    getStats: true
+  aws: AWS,
+  db: "geocore_metadata",
+  formatJson: true,
+  getStats: true,
 };
 const athenaExpress = new AthenaExpress(athenaExpressConfig);
 
 exports.handler = async (event) => {
-    console.log("even is: \n", event)
-    const sqlQuery = query();
+  console.log("even is: \n", event);
+  const sqlQuery = query();
 
-    try {
-        let queryResults = await athenaExpress.query(sqlQuery);
-        const results = queryResults.Items
-        console.log('result is:\n', results)
-        return results;
-    }
-    catch (error) {
-        console.log('result is:\n', error)
-        return error;
-    }
+  try {
+    let queryResults = await athenaExpress.query(sqlQuery);
+    const results = queryResults.Items;
+    console.log("result is:\n", results);
+    return results;
+  } catch (error) {
+    console.log("result is:\n", error);
+    return error;
+  }
 };

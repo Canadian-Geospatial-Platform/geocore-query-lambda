df6604d9ee43c88dc2faf84e3720a6e0dd3ddde5
Pascal Vautour <pascal.vautour@nrcan-rncan.gc.ca>
2022-12-14 14:51:27 -0500

    Add test for single item.

--- components/query-builder/query-builder.test.js
+++ components/query-builder/query-builder.test.js
@@ -8,14 +8,23 @@ test("Empty where clause should be empty string", () => {
   expect(whereClause([])).toBe("");
 });
 
-test("We should receive a valid where request.", () => {
-  const expectedResult = "where regexp_like(properties[1].title.en, '(?i)blood in the water|mountain|nonce')";
+test("We should receive a valid where request when using a single item.", () => {
+  const expectedResult =
+    "where regexp_like(properties[1].title.en, '(?i)mountain')";
+  const keywordsArray = ["mountain"];
+  expect(whereClause(keywordsArray, "en")).toBe(expectedResult);
+});
+
+test("We should receive a valid where request when using multiple items.", () => {
+  const expectedResult =
+    "where regexp_like(properties[1].title.en, '(?i)blood in the water|mountain|nonce')";
   const keywordsArray = ["blood in the water", "mountain", "nonce"];
   expect(whereClause(keywordsArray, "en")).toBe(expectedResult);
 });
 
 test("We should be able to set language code", () => {
-  const expectedResult = "where regexp_like(properties[1].title.fr, '(?i)blood in the water|mountain|nonce')";
+  const expectedResult =
+    "where regexp_like(properties[1].title.fr, '(?i)blood in the water|mountain|nonce')";
   const keywordsArray = ["blood in the water", "mountain", "nonce"];
   expect(whereClause(keywordsArray, "fr")).toBe(expectedResult);
 });
